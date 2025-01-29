9b13ea1af5fe6180f05fe1fb1f33a98a2ed1f038
Pablo Ordóñez <pablosebas61@gmail.com>
2025-01-29 11:45:32 -0500

    Cambios

--- Ejercicios07/src/lectura01/PruebaLeerArchivoTexto.java
+++ Ejercicios07/src/lectura01/PruebaLeerArchivoTexto.java
@@ -5,9 +5,9 @@ public class PruebaLeerArchivoTexto {
 
     public static void main(String args[]) {
         // LeerArchivoTexto.leerRegistros01();
-        // LeerArchivoTexto.leerRegistros02();
-        LeerArchivoTexto.leerRegistros03();
-        // LeerArchivoTexto.leerRegistros04();
+        //LeerArchivoTexto.leerRegistros02();
+        //LeerArchivoTexto.leerRegistros03();
+         LeerArchivoTexto.leerRegistros04();
 
     } // fin de main
 } // fin de la clase PruebaLeerArchivoTexto
